b28df27be890c62efd9a2adb5df4eb5dd2e84cb7
ericson231 <ericsonpascua899@gmail.com>
2024-08-05 23:02:41 +0800

    Update callad.js

--- modules/commands/callad.js
+++ modules/commands/callad.js
@@ -2,14 +2,14 @@ module.exports.config = {
 	name: "callad",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "NTKhang, ManhG Fix Get",
+	credits: "Aki Hayakawa",
 	description: "Report bot's error to admin or comment",
-  usePrefix: true,
-	commandCategory: "report",
+	usePrefix: true,
+	commandCategory: "system",
 	usages: "[Error encountered or comments]",
 	cooldowns: 5
 },
-  
+
 module.exports.handleReply = async function({
 	api: e,
 	args: n,
@@ -22,7 +22,7 @@ module.exports.handleReply = async function({
 		case "reply":
 			var t = global.config.ADMINBOT;
 			for (let n of t) e.sendMessage({
-				body: "📄Feedback from " + i + ":\n" + a.body,
+				body: "Feedback from " + i + ":\n" + a.body,
 				mentions: [{
 					id: a.senderID,
 					tag: i
@@ -38,7 +38,7 @@ module.exports.handleReply = async function({
 			break;
 		case "calladmin":
 			e.sendMessage({
-				body: `📌Feedback from admin ${i} to you:\n--------\n${a.body}\n--------\n»💬Reply to this message to continue sending reports to admin`,
+				body: `Feedback from Admin ${i} to you:\n\n"${a.body}"\n\nReply to this message to continue sending reports to admin`,
 				mentions: [{
 					tag: i,
 					id: a.senderID
@@ -53,6 +53,7 @@ module.exports.handleReply = async function({
 }, module.exports.run = async function({
 	api: e,
 	event: n,
+	Threads,
 	args: a,
 	Users: s,
 	Threads: o
@@ -62,12 +63,12 @@ module.exports.handleReply = async function({
 	var t = n.senderID,
 		d = n.threadID;
 	let r = (await o.getData(n.threadID)).threadInfo;
-	var l = require("moment-timezone").tz("Asia/Manila").format("HH:mm:ss D/MM/YYYY");
-	e.sendMessage(`At: ${l}\nYour report has been sent to the bot admins`, n.threadID, (() => {
+	var l = require("moment-timezone").tz("Asia/Manila").format("MMMM DD, YYYY hh:mm A");
+	e.sendMessage(`${l}\nYour report has been sent to the bot admins`, n.threadID, (async () => {
 		var s = global.config.ADMINBOT;
 		for (let o of s) {
 			let s = r.threadName;
-			e.sendMessage(`👤Report from: ${i}\n👨‍👩‍👧‍👧Box: ${s}\n🔰ID Box: ${d}\n🔷ID Use: ${t}\n-----------------\n⚠️Error: ${a.join(" ")}\n-----------------\nTime: ${l}`, o, ((e, a) => global.client.handleReply.push({
+			e.sendMessage(`${l}\nReport from: ${i}\nUser ID: ${t}\nGroup: ${(await Threads.getInfo(n.threadID)).threadName || "Unknown"}\nGroup ID: ${d}\n\nMessage: ${a.join(" ")}`, o, ((e, a) => global.client.handleReply.push({
 				name: this.config.name,
 				messageID: a.messageID,
 				author: n.senderID,
